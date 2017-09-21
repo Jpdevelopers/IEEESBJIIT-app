9fe0a7ca13781176c759ff76c7fa14544d33ce3f
YugankJr <gargya.shelly@gmail.com>
2017-09-22 03:54:04 +0530

    Added a simple menu with Contact us as option

--- app/src/main/java/com/dev/ieeesbjiit/ieeesbjiit/MainActivity.java
+++ app/src/main/java/com/dev/ieeesbjiit/ieeesbjiit/MainActivity.java
@@ -1,9 +1,13 @@
 package com.dev.ieeesbjiit.ieeesbjiit;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -38,5 +42,34 @@ public class MainActivity extends AppCompatActivity {
         layoutManager= new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_dispaly, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+
+        switch (item.getItemId()) {
+            case R.id.contact: {
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                Uri urid = Uri.parse("mailto:ieeesbjiit62@gmail.com");
+                i.setData(urid);
+                i.putExtra(Intent.EXTRA_SUBJECT, "Complaint/ Suggestion");
+                if (i.resolveActivity(getPackageManager()) != null) {
+                    startActivity(i);
+                }
+                break;
+            }
+        }
+        return true;
     }
 }
